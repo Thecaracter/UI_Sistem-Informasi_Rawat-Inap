d0b25306f06f5181b021b3272c061bec9c2ab648
thecaracter <92218072+Thecaracter@users.noreply.github.com>
2023-06-16 21:14:13 +0700

    penambahan dan perbaikan di splash

--- lib/splash.dart
+++ lib/splash.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ui_sistem_rawat_inap/onboarding.dart';
 
 class Splash extends StatefulWidget {
   const Splash({Key? key}) : super(key: key);
@@ -8,6 +9,18 @@ class Splash extends StatefulWidget {
 }
 
 class _SplashState extends State<Splash> {
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => OnBoarding(),
+        ),
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -39,6 +52,7 @@ class _SplashState extends State<Splash> {
                   ),
                 ),
               ),
+
               Column(
                 children: <Widget>[
                   Container(
@@ -47,6 +61,43 @@ class _SplashState extends State<Splash> {
                       "assets/logo.png",
                       width: MediaQuery.of(context).size.width / 1,
                       height: MediaQuery.of(context).size.height / 3,
+                    ),
+                  ),
+                  Container(
+                    child: Text(
+                      "SIRANAP",
+                      style: TextStyle(
+                        decoration: TextDecoration.none,
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        shadows: [
+                          Shadow(
+                            color: Colors.black.withOpacity(0.1),
+                            blurRadius: 10,
+                            offset: Offset(
+                                0, 8), // Adjust the offset values as needed
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Align(
+                    alignment: Alignment.center,
+                    child: Center(
+                      child: Text(
+                        "Sistem Informasi Rawat Inap",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          decoration: TextDecoration.none,
+                          fontSize: 12,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black.withOpacity(0.5),
+                        ),
+                      ),
                     ),
                   ),
                   SizedBox(
